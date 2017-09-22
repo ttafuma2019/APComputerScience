0572b8952f0086f66c2db7560fc958cf4002c6a2
ttafuma2019 <ttafuma2019@cchsraider.net>
2017-09-22 10:08:31 -0400

    initial commit

---
+++ Problem1/src/Problem1.java
@@ -0,0 +1,19 @@
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		longLine(); 
+		victory(); 
+		longLine(); 
+		
+	public static void longLine();{
+		System.out.println("//////////////////////");
+	}
+	public static void victory();{
+		System.out.println("||Victory is mine!||");
+	}
+
+	}
+
+}
